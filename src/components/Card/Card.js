d7cd74ab088c153d027ac6e1d1793281977fe072
Merto <mertenercan@gmail.com>
2022-07-09 01:14:23 +0300

    Added dot menu icon and status dot section

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,19 +3,20 @@ import ProgressBar from "./ProgressBar";
 import Percent from "./Percent";
 
 function Card(props){
-
+    let dotMenu="dot-menu"
     let progressBar =""
     if(props.status == "Active" || props.status=="Inactive"){
         progressBar="card-progress"
     }else{
         progressBar="card-progress-2"
+        dotMenu="dot-menu-onhold"
     }
 
     return (
         <div className="card">
             <CardInfo date={props.date} status={props.status} tasks={props.tasks}
             users={props.users}/>
-        
+            <img src={require("./Icons/Vector.png")} className={dotMenu}></img>
             <div className={progressBar}>
             <div className="card-members">
             +
--- src/components/Card/CardInfo.js
+++ src/components/Card/CardInfo.js
@@ -7,12 +7,17 @@ function CardInfo(props){
     let className = ""
     let status=""
     let infoStatus="card-info-2";
+    let statusDot="status-dot"
+    let cardData="card-data"
     if(props.status =="On Hold"){
         className = "on-hold"
         status="Status"
         infoStatus="card-info"
+        statusDot="status-dot-onhold"
+        cardData="card-data-onhold"
     }else if (props.status=="Active"){
     className="active"
+    statusDot="status-dot-active"
     
     }else{
         className="inactive"
@@ -32,12 +37,15 @@ function CardInfo(props){
                 <h4 className="started">Started</h4>
                 <p className="started-date">{props.date}</p>
                 <h4 className="status" >{status}</h4>
-                <ul>
-                    <li className={className}>{props.status}</li>
-                </ul>
+
+
+                <div className={statusDot}></div>
+             
+                    <h2 className={className}>{props.status}</h2>
+                
             </div>
 
-            <div className="card-data">
+            <div className={cardData}>
                 <div className="data-info-1">
                 <h2 className="data-number">{props.tasks}</h2>
                 <p className="data-text">Tasks</p>
