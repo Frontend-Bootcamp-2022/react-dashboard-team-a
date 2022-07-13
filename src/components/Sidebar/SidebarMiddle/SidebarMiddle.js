a0ee95759774c75cadb797d8104afcc0d70009c8
sarupu <sarp.tolunay@gmail.com>
2022-07-13 22:27:02 +0300

    Update: Logic change in SidebarMiddle

--- src/components/Sidebar/SidebarMiddle/SidebarMiddle.js
+++ src/components/Sidebar/SidebarMiddle/SidebarMiddle.js
@@ -1,49 +1,15 @@
 import React from "react";
-import {
-  IconDashboard,
-  IconMembers,
-  IconModules,
-  IconProjects,
-  IconReports,
-  IconSprint,
-} from "../../../assets/icons";
+import sidebarNavItems from "../../../assets/SidebarNavItems/SidebarNavItems"
+import SidebarMiddleItem from "./SidebarMiddleItems/SidebarMiddleItems";
 
 function SidebarMiddle() {
+  const itemNotLogout = sidebarNavItems.map((item) => item.display !== "Log out" ? 
+      <SidebarMiddleItem display={item.display} icon={item.icon} /> : 
+      false)
+
   return (
     <div className="sidebarMiddleBox">
-      <div className="sidebarContentItem" role="button">
-        <IconDashboard />
-
-        <span className="sidebarContentText"> Dashboard</span>
-      </div>
-
-      <div className="sidebarContentItem" role="button">
-        <IconProjects className="sidebar-icon" />
-
-        <span className="sidebarContentText">Project</span>
-      </div>
-      <div className="sidebarContentItem" role="button">
-        <IconModules />
-
-        <span className="sidebarContentText">Modules</span>
-      </div>
-      <div className="sidebarContentItem" role="button">
-        <IconSprint />
-
-        <span className="sidebarContentText">Sprint</span>
-      </div>
-
-      <div className="sidebarContentItem" role="button">
-        <IconMembers />
-
-        <span className="sidebarContentText">Members</span>
-      </div>
-
-      <div className="sidebarContentItem" role="button">
-        <IconReports />
-
-        <span className="sidebarContentText"> Peports</span>
-      </div>
+      {itemNotLogout}
     </div>
   );
 }
